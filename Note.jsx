cb556913d826a227fe26926989ff267f716e742a
Aliyabanu <85398209+Aliyabanu@users.noreply.github.com>
2021-06-06 20:14:53 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JS & React Bootcamp - Shape AI</h1>
+      <p>
+        I have learnt new things in bootcamp conducted by shape AI by
+        ShauryaSinha and I'm looking forward To learn More.. ^_^
+      </p>
+    </div>
+  );
+}
+
+export default Note;
